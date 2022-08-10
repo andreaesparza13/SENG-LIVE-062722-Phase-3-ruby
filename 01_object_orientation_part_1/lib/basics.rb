5e9ade2f29a9a54d84e5daa96be4d6af46d6362e
Andrea <acesparza05@gmail.com>
2022-08-10 18:08:04 -0500

    lesson 1

---
+++ 01_object_orientation_part_1/lib/basics.rb
@@ -0,0 +1,13 @@
+require 'pry'
+#Ruby Data Types
+    #String
+    #Number - Integers and Floats
+    #booleans
+    #Array
+    #Symbols
+    #Variables
+    name = 'Lady'
+    puts "my dog #{name} is cute!"
+    #Hash
+    myHash = {dog: 'Lady'}
+binding.pry
--- 01_object_orientation_part_1/lib/dog.rb
+++ 01_object_orientation_part_1/lib/dog.rb
@@ -1,6 +1,16 @@
 class Dog
-  # ✅ we should be able to create dogs with a name, age, breed, and image_url
+  attr_reader :name, :breed
+  attr_accessor :age, :image_url, :last_fed_at, :last_walked
 
+  # ✅ we should be able to create dogs with a name, age, breed, and image_url
+  def initialize(name, age, breed, image_url, last_walked, last_fed_at)
+    @name = name
+    @age = age
+    @breed = breed
+    @image_url = image_url
+    @last_fed_at = last_fed_at
+    @last_walked = last_walked
+  end
   
 
   # ✅ we want to be able to access a dog's name, age, breed, image_url, last_fed_at and last_walked at times
