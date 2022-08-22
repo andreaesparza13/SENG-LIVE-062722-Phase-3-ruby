22462ecdbe92d2f28c883578b78595aba29eadab
Andrea <acesparza05@gmail.com>
2022-08-22 13:46:42 -0500

    deleted 07

--- 07_web_api_with_sinatra/api/app/models/handler.rb
+++
@@ -1,4 +0,0 @@
-class Handler < ActiveRecord::Base
-  has_many :requests 
-  has_many :pets, through: :requests
-end
--- 07_web_api_with_sinatra/api/app/models/pet.rb
+++
@@ -1,5 +0,0 @@
-class Pet < ActiveRecord::Base 
-  belongs_to :owner
-  has_many :requests 
-  has_many :handlers, through: :requests  
-end
