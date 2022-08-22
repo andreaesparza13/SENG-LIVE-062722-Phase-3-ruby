cbaac34b762c38a004f8c26746ab154a27a986e4
Andrea <acesparza05@gmail.com>
2022-08-22 14:08:30 -0500

    undo previous changes

--- 05_intro_to_activerecord/lib/pet.rb
+++ 05_intro_to_activerecord/lib/pet.rb
@@ -1,28 +1,28 @@
 class Pet < ActiveRecord::Base
-   # CRUD
-   #    Create
-   #       Pet.create -> creates a new instance and saves it to the database
-   #          Which does the same thing as:
-   #             Pet.new -> create a new instance
-   #             Pet.save -> save the instance to the database
+   #CRUD
+      #Create
+         #Pet.create -> creates a new instance and saves it to the database
+            #Which does the same thing as:
+               #Pet.new -> create a new instance
+               #Pet.save -> save the instance to the database
 
-   #    Read
-   #       Pet.all -> lists all instances in the database
-   #       Pet.find -> returns an instance by id
-   #       Pet.find_by -> returns an instance by an attribute
-   #       Pet.first -> returns first resource
-   #       Pet.last -> returns last resource
-   #       Pet.where(:attribute = 'something')
-   #       Pet.order(:attribute)
-   #       Pet.pluck(:attribute)
-   #       So many more!
+      #Read
+         #Pet.all -> lists all instances in the database
+         #Pet.find -> returns an instance by id
+         #Pet.find_by -> returns an instance by an attribute
+         #Pet.first -> returns first resource
+         #Pet.last -> returns last resource
+         #Pet.where(:attribute = 'something')
+         #Pet.order(:attribute)
+         #Pet.pluck(:attribute)
+         #So many more!
 
-   #    Update
-   #       pet_instance.update -> update the instance
-   #       Pet.update -> updates ALL instances
-   #       pet_instance.save -> save changes to the instance
+      #Update
+         #pet_instance.update -> update the instance
+         #Pet.update -> updates ALL instances
+         #pet_instance.save -> save changes to the instance
 
-   #    Delete
-   #       pet_instance.destroy -> destroy the instance
-   #       Pet.destroy -> destroy ALL instances
+      #Delete
+         #pet_instance.destroy -> destroy the instance
+         #Pet.destroy -> destroy ALL instances
 end
